8bf690c03a6f83bc547d04053d3b5cae561179e2
luchiano9 <67933124+luchiano9@users.noreply.github.com>
2020-10-07 17:19:48 -0300

    Update ViajanteMilena.py

--- ViajanteMilena.py
+++ ViajanteMilena.py
@@ -5,35 +5,39 @@ import pandas as pd
 import os
 
 
-path = 'C:/UTN 2020/Alg. geneticos/viajante/datos.xlsx'
+path = 'C:/Users/Luciano/Desktop/elViajante-main/datos.xlsx'
 df = pd.read_excel(path) 
 # df contiene los datos de ciudades y distancias
 
 recorrido = []
 distancias = []
 
-df = df.set_index('name')
+df = df.set_index('Distancias en kilómetros')  # columna indice
 
-#aca esta hardcodeado la ciudad de inicio, y eliminar la fila de esa ciudad de inicio
-ciudadini = 'San Luis'
-df = df.drop(['San Luis'])
-recorrido.append(ciudadini)
+# aca esta hardcodeado la ciudad de inicio, y eliminar la fila de esa ciudad de inicio
+ciudadIni = 'San Luis'
+df = df.drop(['San Luis'])  #elimina fila de la ciudad
+recorrido.append(ciudadIni)
 
 def getRecorrido(recorrido=recorrido,distancias=distancias,df=df):
-    if (len(recorrido)==len(df.columns)): 
-        recorrido.append(sum(distancias))
-        recorrido.append(ciudadini)
+    if (len(recorrido)==len(df.columns)):  # si entra por true es porque ya termino , parece que no da true nunca
+        print(sum(distancias))
+        # recorrido.append(sum(distancias)) para que sirve esta linea?
+        # recorrido.append(ciudadIni) para que sirve esta linea?
         return recorrido
     else:
         for col in df.columns:
+            print(col)
             if recorrido[len(recorrido)-1] == col: # busca columna de el ultimo añadido
-                mascercana = df[col].idxmin()
-                distancias.append(df[col][mascercana]) 
-                recorrido.append(mascercana)
-                df = df.drop([mascercana])
+                masCercana = df[col].idxmin()  # idxmin devuelve el valor minimo de cada columna
+                distancias.append(df[col][masCercana]) 
+                recorrido.append(masCercana)
+                df = df.drop([masCercana])
                 getRecorrido(recorrido, distancias, df)
-                break;
+                
+                break
                 
 
+                
 getRecorrido()
 print("R: ", recorrido)
