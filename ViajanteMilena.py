34bfacc7cbea6c5256d3f1c1eb5275216cb1de55
luchiano9 <67933124+luchiano9@users.noreply.github.com>
2020-10-07 19:28:21 -0300

    Update ViajanteMilena.py

--- ViajanteMilena.py
+++ ViajanteMilena.py
@@ -11,33 +11,50 @@ df = pd.read_excel(path)
 
 recorrido = []
 distancias = []
+i=0
 
 df = df.set_index('Distancias en kilómetros')  # columna indice
 
 # aca esta hardcodeado la ciudad de inicio, y eliminar la fila de esa ciudad de inicio
-ciudadIni = 'San Luis'
-df = df.drop(['San Luis'])  #elimina fila de la ciudad
-recorrido.append(ciudadIni)
+# ciudadIni = 'San Luis'
+# df = df.drop(['San Luis'])  #elimina fila de la ciudad
+# recorrido.append(ciudadIni)
 
-def getRecorrido(recorrido=recorrido,distancias=distancias,df=df):
-    if (len(recorrido)==len(df.columns)):  # si entra por true es porque ya termino , parece que no da true nunca
-        print(sum(distancias))
-        # recorrido.append(sum(distancias)) para que sirve esta linea?
-        # recorrido.append(ciudadIni) para que sirve esta linea?
+def menu(df=df):
+    print("El problema del viajante")
+    print("Ingrese el nombre de la ciudad de inicio")
+    ciudadIni = input()
+    recorrido.append(ciudadIni)
+    return ciudadIni
+
+def getRecorrido(ciudadIni,i=i,recorrido=recorrido,distancias=distancias,df=df):
+    if i==0:
+        print("hola")               # El bloque de aca es lo unico que se me ocurrio cuando me empezo a dar errores el drop de la ciudad inicial
+        df = df.drop(ciudadIni) 
+        i = 1
+    if (len(recorrido)==len(df.columns)):  # si entra por true es porque ya termino
+
+        recorrido.append(ciudadIni) 
+        recorrido.append(sum(distancias)) 
+        
         return recorrido
     else:
         for col in df.columns:
-            print(col)
+           
             if recorrido[len(recorrido)-1] == col: # busca columna de el ultimo añadido
                 masCercana = df[col].idxmin()  # idxmin devuelve el valor minimo de cada columna
-                distancias.append(df[col][masCercana]) 
+                distancias.append(df[col][masCercana])
                 recorrido.append(masCercana)
+                if (len(recorrido)==len(df.columns)):           #Este bloque es para agregar la distancia al volver a la ciudad de inicio
+                    for colu in df.columns:
+                        if colu == ciudadIni:
+                            distFinal = df[colu][masCercana]
+                            distancias.append(distFinal)
+
                 df = df.drop([masCercana])
-                getRecorrido(recorrido, distancias, df)
-                
+                getRecorrido(ciudadIni,i,recorrido, distancias, df)                              
                 break
                 
-
-                
-getRecorrido()
+ciudadIni = menu()               
+getRecorrido(ciudadIni)
 print("R: ", recorrido)
